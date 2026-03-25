4d9f8f162c58004a7086f98014f69d337b518ff5
XFeng111 <168633853+XFeng111@users.noreply.github.com>
2026-03-25 21:31:13 +0800

    Update T_Event_Creat_FromActorMixer.py

--- Tools/T_Event_Creat_FromActorMixer.py
+++ Tools/T_Event_Creat_FromActorMixer.py
@@ -5,6 +5,8 @@ from WAAPI_Functions import Core_object, Core_undo, Ui
 from T_ActiveSource_Loop import Source_SetLoop
 
 def T_Event_Creat_FromActorMixer():
+    index = input("输入y —— 从已有资源的Event结构创建，若无Event结构则按照ActorMixer创建\n输入n —— 创建至默认路径Events\n等待输入ing...")
+
     # 初始化类，传入client
     c_obj = Core_object(client)
     ui = Ui(client)
@@ -36,15 +38,33 @@ def T_Event_Creat_FromActorMixer():
                     print(f"{o_name}是ActorMixer或Folder，已跳过新建Event\n")
                 else:
                     if o_refer == {'return': [{}]}:
-                        # 创建事件
-                        c_obj.play_event_create(o_name, o_id)
-                        # 若_loop或_lp结尾，则继续创建停止事件，并为Source设置Loop
-                        if o_name.lower().endswith(("_loop", "_lp")):
-                            # 创建停止事件
-                            c_obj.stop_event_create(o_name, o_id)
-                            # 若为Sound，为Source设置Loop
-                            if o_type == "Sound":
-                                Source_SetLoop(c_obj, o_id)
+                        if index.lower() == "n":
+                            # 默认路径创建事件
+                            c_obj.play_event_create(o_name, o_id)
+                            # 若_loop或_lp结尾，则继续创建停止事件，并为Source设置Loop
+                            if o_name.lower().endswith(("_loop", "_lp")):
+                                # 创建停止事件
+                                c_obj.stop_event_create(o_name, o_id)
+                                # 若为Sound，为Source设置Loop
+                                if o_type == "Sound":
+                                    Source_SetLoop(c_obj, o_id)
+                        elif index.lower() == "y":
+
+                            client.call("ak.wwise.core.undo.endGroup", {"displayName": "Before Creat Work Unit"})
+                            # ------------------------------------------------
+                            c_obj.event_creat_FromActorPath(o_name, o_id, 1) # WorkUnit 会打断前面的 undo 组，补一组防报错
+                            # ------------------------------------------------
+                            client.call("ak.wwise.core.undo.beginGroup")
+
+                            if o_name.lower().endswith(("_loop", "_lp")):
+                                c_obj.event_creat_FromActorPath(o_name, o_id, 2)
+                                if o_type == "Sound":
+                                    Source_SetLoop(c_obj, o_id)
+
+                        else:
+                            print("❌ 输入有误，请重新运行脚本")
+                            return
+
                     else:
                         o_refer_count = len(o_refer["return"][0]["referencesTo"])
                         print(f"{o_name}已被{o_refer_count}个对象引用，已跳过新建Event\n")
@@ -58,8 +78,8 @@ if __name__ == "__main__":
     else:
         # 开始某个 Undo Group
         c_undo = Core_undo(client)
-        c_undo.undo_beginGroup()
 
+        c_undo.undo_beginGroup() # 涉及创建 Work Unit 会打断undo组
         T_Event_Creat_FromActorMixer()
 
         # 结束某个 Undo Group
